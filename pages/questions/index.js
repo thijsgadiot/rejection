6bd9f3cd070d953ce2f5a1207cd64bfa2ebeb471
Thijs Gadiot <thijs@gadiot.net>
2021-02-08 12:30:45 +0100

    chore: clean up index page, no logic

--- pages/questions/index.js
+++ pages/questions/index.js
@@ -1,21 +1,16 @@
-import { useQuery } from "@apollo/client";
 import Head from "next/head";
 import { QuestionContainer } from "../../src/components/questionContainer";
-import TotalScore from "../../src/components/totalScore";
+import { TotalScoreContainer } from "../../src/components/totalScoreContainer";
 import AddQuestion from "../../src/components/addQuestion";
-import { QUESTIONS } from "../../src/queries";
 
 export default function Questions() {
-  const { data } = useQuery(QUESTIONS);
-
   return (
     <div className="question-container">
       <Head>
         <title>Questions</title>
       </Head>
 
-      {data && <TotalScore score={data.questions.data.reduce((a, c) => a + c.score, 0)} />}
-
+      <TotalScoreContainer />
       <AddQuestion />
       <QuestionContainer />
     </div>
